9d3ea7730b4d6d3e4f82b0761dcd35f0b13fcc90
Shashank K Prabhu <shashankkp02@gmail.com>
2024-01-31 23:18:46 +0530

    database

---
+++ database/db.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI;
+
+const connectToMongo =()=>{
+  mongoose.connect(mongoURI)
+  .then(()=>{
+    console.log("Connected to MongoDB")
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+}
+
+module.exports=connectToMongo;
